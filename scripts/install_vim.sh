3846388e142303272f92c4b9f250e66039c9fd85
root <root@generic101.webapp.phx1.mozilla.com>
2011-11-17 05:59:16 -0800

    Added install script to build vim from source

---
+++ scripts/install_vim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+bunzip2 vim-7.3.tar.bz2
+tar vm-7.3.tar
+
+cd vim73
+
+#For RHEL
+#yum install python-devel
+#yum install ruby-devel
+#yum install ncurses-devel
+
+#For Debian
+#apt-get install python-dev
+#apt-get install ruby-dev
+#apt-get install ncurses-dev
+
+#On rhel systems you need the following
+# ./configure --prefix=/usr/local --enable-rubyinterp --enable-pythoninterp --with-features=huge --with-python-config-dir=/usr/lib64/python2.6/config
+#The following should work on debian based
+./configure --prefix=/usr/local --enable-rubyinterp --enable-pythoninterp --with-features=huge
+make
+make install
